db159c32790bc863d233cb3a10769fb3d2c5f7be
Ian Philip Eglin <philip@eglin.no>
2021-04-04 23:22:08 +0200

    Added a remove message commant that removes the warn line that the caller posted

--- commands/moderation/warn.js
+++ commands/moderation/warn.js
@@ -16,6 +16,11 @@ module.exports = {
             }
         }
         warningTag = warningTag.slice(0, -1);
+
+        message.channel.bulkDelete(1, true).catch(err => {
+            console.error(err);
+            message.channel.send("There was an error trying to prune messages in this channel!");
+        });
         
         message.channel.send(`**${taggedUser.username}** have been warned for: *${warningTag}*`)
     }
